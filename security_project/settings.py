8d40d10e142a843ce475fd2a1123585c1b3190c2
piyush <piyushbussiness@gmail.com>
2025-11-22 15:53:20 +0530

    Security hardening: Moved SECRET_KEY to environment variables

--- security_project/settings.py
+++ security_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*kx&ccehx-i(64j(y)yf1g3)o&%6kkv$tycxe-31(72qphj&xf'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
